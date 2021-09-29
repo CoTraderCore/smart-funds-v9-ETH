30a964df8530dc18bda41ca7f4e55f4aa28bfe01
RuslanMirov <ruslan777mirov@gmail.com>
2021-09-29 12:58:49 +0300

    Fix price portal UNI

--- contracts/core/portals/PricePortalUniswap.sol
+++ contracts/core/portals/PricePortalUniswap.sol
@@ -126,7 +126,7 @@ contract PricePortalUniswap is Ownable {
     returns (uint256)
   {
     uint256[] memory res = Router(router).getAmountsOut(fromAmount, path);
-    return res[1];
+    return res[path.length - 1];
   }
 
   // owner can add common connectors
